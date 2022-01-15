c30d87ffd7f0db47e20cbd1e5e1d54077cf4a96c
pisuthd <pisuth.dae@gmail.com>
2022-01-15 17:51:23 +0700

    setup hardhat project

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,40 @@
+require("dotenv").config()
+
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.6.12",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize :true,
+      forking: {
+        url: process.env.POLYGON_URL,
+        blockNumber: 20480699,
+      },
+    },
+  },
+  mocha: {
+    timeout: 1200000,
+  }
+};
